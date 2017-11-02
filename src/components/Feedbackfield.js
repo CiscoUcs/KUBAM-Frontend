9c9b2f51abbb933392bdbb12f7e273ddb8de6a1f
larabenito <lbenitoc@cisco.com>
2017-11-02 17:20:13 +0100

    Added Feedback - first version

---
+++ src/components/Feedbackfield.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+var Feedbackfield = ({selected, onClick}) => (
+
+);
+
+Feedbackfield.propTypes = {
+  onClick: PropTypes.func.isRequired,
+}
+
+export default Feedbackfield
